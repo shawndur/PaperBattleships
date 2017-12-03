7d09b3645ce7d7fe33e14d132c197f3fcc707254
shawn <shawndur@buffalo.edu>
2017-12-03 12:35:14 -0500

    Added message box component

---
+++ src/components/MessageBox.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MessageBox = (props) => {
+    return <h3>Message Box</h3>
+}
+
+MessageBox.propTypes = {
+    message: PropTypes.string
+}
+
+export default MessageBox;
