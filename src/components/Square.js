bcd21a0dd8bbb2c246db30ca9732e8570a5c898f
shawn <shawndur@buffalo.edu>
2017-12-04 20:43:50 -0500

    remove grid stuff from square

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,29 +1,12 @@
 import React from "react";
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 import '../css/Square.css'
 
 const Square = (props) => {
-    const row = props.row;
-    const column = props.column;
-    const styles = {
-        gridArea: row + " / " + column + " / span 1 / span 1"
-    }
     return (
-        <div className="Square" style={styles}>
+        <div className="Square">
         </div>
     )
-}
-
-Square.propTypes = {
-    row: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number
-    ]).isRequired,
-      
-    column: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.number
-    ]).isRequired
 }
 
 export default Square;
