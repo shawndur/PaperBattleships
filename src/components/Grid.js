dc7beb0988ff126ba377df8d30e3f8ec6daa0fb7
shawn <shawndur@buffalo.edu>
2017-12-04 20:45:14 -0500

    remove square creation from grid

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Square from './Square';
 import '../css/Grid.css'
 
 const Grid = (props) => {
@@ -9,16 +8,11 @@ const Grid = (props) => {
     const styles = {
         gridTemplate: "repeat("+rows+",1fr) / repeat("+columns+",1fr)"
     };
-    const squares = [];
-    for (let i=1; i<=rows; ++i) {
-        for (let j=1; j<=columns; ++j) {
-            squares.push(<Square key={"grid-square:"+i+","+j} row={i} column={j}/>);
-        }
-    }
+    
     return (
         <div className="Grid-container">
             <div className="Grid" style={styles}>
-                {squares}
+                {props.children}
             </div>
         </div>
     )
