24fd1970ff7b2c9097ea3a78354162c365a4a4c3
shawn <shawndur@buffalo.edu>
2017-12-04 20:43:19 -0500

    Create a grid item that positions items on grid

---
+++ src/components/GridItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GridItem = (props) => {
+    const startRow = props.pos.row[0];
+    const startCol = props.pos.col[0];
+    const endRow = (props.pos.spanRow ? "span " : "") + props.pos.row[1];
+    const endCol = (props.pos.spanCol ? "span " : "") + props.pos.col[1];
+    const styles = {
+        gridArea: `${startRow} / ${startCol} / ${endRow} / ${endCol}`
+    }
+    return (
+        <div style={styles}>
+            {props.children}
+        </div>
+    );
+}
+
+const tuplePropType = (props, propName, componentName) => {
+    const prop = props[propName];
+    if(!Array.isArray(prop) || prop.length !== 2 || !prop.every(Number.isInteger)) {
+        return new Error(`${propName} must be an array of 2 integers`);
+    }
+}
+
+GridItem.propTypes = {
+    pos: PropTypes.shape({
+        row: tuplePropType.isRequired,
+        col: tuplePropType.isRequired,
+        spanRow: PropTypes.bool.isRequired, 
+        spanCol: PropTypes.bool.isRequired
+    }).isRequired
+}
+
+export default GridItem
