272830856e970daa8bb9d38d12a1289d68072f1b
shawn <shawndur@buffalo.edu>
2017-12-04 20:44:55 -0500

    create a board component that creates a grid and places squares wrapped in grid items in the grid

---
+++ src/components/Board.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import Grid from './Grid';
+import GridItem from './GridItem';
+import Square from './Square';
+
+const Board = (props) => {
+    const rows = props.rows;
+    const columns = props.columns;
+    const squares = [];
+    for (let i=1; i<=rows; ++i) {
+        for (let j=1; j<=columns; ++j) {
+            const pos = {
+                row: [i,1],
+                col: [j,1],
+                spanRow: true,
+                spanCol: true
+            }
+            squares.push(
+                <GridItem key={"grid-square:"+i+","+j} pos={pos}>
+                    <Square />
+                </GridItem>
+            );
+        }
+    }
+
+    return (
+        <Grid rows={rows} columns={columns}>
+            {squares}
+        </Grid>
+    )
+}
+
+Board.propTypes = {
+    rows: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]).isRequired,
+      
+    columns: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]).isRequired
+}
+
+export default Board;
