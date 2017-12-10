a403d58b41d39f5a65dddebc6dbad6f3c1f487e6
shawn <shawndur@buffalo.edu>
2017-12-10 11:20:40 -0500

    fixed circle responsiveness

--- src/components/markers/Circle.js
+++ src/components/markers/Circle.js
@@ -3,9 +3,11 @@ import '../../css/Circle.css'
 
 const Circle = (props) => {
     return (
-        <svg className='Circle' height='100' width='100'>
-            <circle cx='50%' cy='50%' r='25%' stroke='black' strokeWidth='3' fill={props.color}/>
-        </svg>
+        <div className='Circle'>
+            <svg height='100' width='100'>
+                <circle cx='50%' cy='50%' r='25%' stroke='black' strokeWidth='5%' fill={props.color}/>
+            </svg>
+        </div>
     );
 }
 
