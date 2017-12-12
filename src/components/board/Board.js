4ffbd418128a94b748c2bbc54f22eb6a47537df8
shawn <shawndur@buffalo.edu>
2017-12-11 20:53:56 -0500

    remove grid

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types'
-import Grid from '../grid/Grid';
 import '../../css/Board.css'
 
 const Board = (props) => {
@@ -8,18 +7,23 @@ const Board = (props) => {
     for (let i=1; i<=props.rows; ++i) {
         for (let j=1; j<=props.columns; ++j) {
             squares.push(
-                <div className='square' key={`%{i},%{j}`} 
-                     style={{ gridArea: `${i} / %{j} / span 1 / span 1` }}/>
+                <div 
+                    className='square' key={i+','+j} 
+                    style={{ gridArea: `${i} / %{j} / span 1 / span 1` }}
+                />
             );
         }
     }
 
     return (
-        <div className='Board'>
-            <Grid rows={props.rows} columns={props.columns}>
+        <div className='Board-container'>
+            <div 
+                className='Board' 
+                style={{gridTemplate: `repeat(${props.rows},1fr) / repeat(${props.columns},1fr)`}}
+            >
                 {squares}
                 {props.children}
-            </Grid>
+            </div>
         </div>
     )
 }
