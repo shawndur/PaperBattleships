e39507525aa33bce7f728991e95509d924691b0f
shawn <shawndur@buffalo.edu>
2017-12-19 22:35:41 -0500

    add game to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+
+import Game from './containers/Game';
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Game />
       </div>
     );
   }
