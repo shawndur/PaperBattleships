e3300a2c6d03f586b00a8ee055019c2abcf037c7
shawn <shawndur@buffalo.edu>
2017-12-20 20:43:42 -0500

    add ongameend

--- src/containers/Game.jsx
+++ src/containers/Game.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import Enemy from  './Enemy';
 import Player from './Player';
 import MessageBox from '../components/MessageBox';
@@ -49,6 +50,10 @@ class Game extends Component {
             </div>
         );
     }
+}
+
+Game.propTypes = {
+    onGameEnd: PropTypes.func.isRequired
 }
 
 export default Game;
