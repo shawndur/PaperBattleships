74f54ab88a1ab2887338d891589705dcc16930b4
shawn <shawndur@buffalo.edu>
2017-12-28 22:38:28 -0500

    change board to a pure component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import Squares from './Squares';
 import '../css/Board.css';
 
 /**
- * Stateless functional component that renders a game board
- * @param {*} props 
+ * Board component that renders a game board
  */
-const Board = (props) => {
-    const {rows, cols} = props.gameConfig.boardSize;
+class Board extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-    //wrapper for onClick, splits the key of a clicked square 
-    // and calls on click passing the row and col
-    const handleClick = props.onClick ? (e) => {
-        const coord = e.target.id.split(',');
-        props.onClick(parseInt(coord[0], 10), parseInt(coord[1], 10));
-    } : undefined;
-    
-    return (
-        <div className='Board-container'>
-            <div 
-                className='Board' 
-                style={{gridTemplate: `repeat(${rows},1fr) / repeat(${cols},1fr)`}}
-                onClick={handleClick}
-            >
-                <Squares rows={rows} cols={cols}/>
-                {props.children}
+    handleClick(e) {
+        if (this.props.onClick) {
+            const coord = e.target.id.split(',');
+            this.props.onClick(parseInt(coord[0], 10), parseInt(coord[1], 10));
+        }
+    }
+
+    render() {
+        const {rows, cols} = this.props.gameConfig.boardSize;
+        const handleClick = this.props.onClick ? this.handleClick : undefined;
+        
+        return (
+            <div className='Board-container'>
+                <div 
+                    className='Board' 
+                    style={{gridTemplate: `repeat(${rows},1fr) / repeat(${cols},1fr)`}}
+                    onClick={handleClick}
+                >
+                    <Squares rows={rows} cols={cols}/>
+                    {this.props.children}
+                </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 Board.propTypes = {
     gameConfig: PropTypes.shape({
