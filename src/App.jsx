3ea8ec4001ab3e8968657ee45980d9ec689704ae
shawn <shawndur@buffalo.edu>
2017-12-31 17:30:24 -0500

    remove template items

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './css/App.css';
 
 import Game from './containers/Game';
@@ -76,13 +75,6 @@ class App extends Component {
 
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
                 {game}
             </div>
         );
